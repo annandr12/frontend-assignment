2ab3f204d12808043d6d13596b6f12dbc2336ce9
bannandr <annandr12@gmail.com>
2019-07-10 16:42:44 +0200

    add api requests data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,76 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:6010';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      filters: ['fashion', 'sports'],
+      selectedFilters: [],
+      sort: null,
+      articles: []
+    }
+  }
+
+  getArticlesRequest(category) {
+    return axios.get(API_URL + '/articles/' + category);
+  }
+
+  getArticles() {
+    const {selectedFilters, filters} = this.state;
+    const requests = selectedFilters.length ?
+      selectedFilters.map((filter) => this.getArticlesRequest(filter)) :
+      filters.map((filter) => this.getArticlesRequest(filter));
+
+    axios.all(requests)
+      .then(axios.spread((fashion, sports) => {
+        this.setState({
+          isLoaded: true,
+          articles: [...fashion.data.articles, ...sports.data.articles]
+        })
+      }))
+      .catch((err)=> {
+        if(err.response.status < 500) {
+          this.setState({
+            isLoaded: true,
+            error: err.response.data
+          })
+        } else {
+          this.setState({
+            isLoaded: true,
+            error: { message: 'Something went wrong' }
+          })
+        }
+      });
+  }
+
+  componentDidMount() {
+    this.getArticles();
+  }
+
   render() {
-    return (
-      <div>Hello world</div>
-    );
+    const { error, isLoaded, articles } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      console.log(articles);
+      return (
+        <ul>
+          {articles.map(item => (
+            <li key={item.id}>
+              {item.title} {item.preamble}
+            </li>
+          ))}
+        </ul>
+      );
+    }
   }
 }
 
